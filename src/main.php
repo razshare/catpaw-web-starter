22f664f0e1286f7910932baa1f77db2e1abc6766
Razvan Tanase <tangent.jotey@gmail.com>
2022-09-21 01:29:06 +0200

    Version 0.4.20

--- src/main.php
+++ src/main.php
@@ -6,7 +6,6 @@ use CatPaw\Web\Utilities\Route;
 
 #[StartWebServer]
 function main() {
-    Route::get('/test', fn() => "this is a test");
     Route::get("/openapi", fn(OpenAPIService $oa) => $oa->getData());
     echo Route::describe().PHP_EOL;
 }
