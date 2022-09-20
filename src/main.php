56ab40cbd955d5f95f07f354a825d5cd94450939
Razvan Tanase <tangent.jotey@gmail.com>
2022-09-21 01:09:53 +0200

    Version 0.4.18

--- src/main.php
+++ src/main.php
@@ -1,58 +1,12 @@
 <?php
 
-use CatPaw\Web\Attributes\ProducedResponse;
-use CatPaw\Web\Attributes\Produces;
 use CatPaw\Web\Attributes\StartWebServer;
 use CatPaw\Web\Services\OpenAPIService;
 use CatPaw\Web\Utilities\Route;
 
 #[StartWebServer]
 function main() {
-    Route::get(
-        '/test',
-        #[Produces(
-            new ProducedResponse(
-                type: 'application/json',
-                status: 200,
-                schema: [
-                    "user" => [[        // <=== note the double wrapping
-                        "email"    => "string",
-                        "name"     => "string",
-                        "articles" => [[        // <=== double wrapping again to indicate an array of articles
-                            "title"       => "string",
-                            "description" => "string",
-                        ]]
-                    ]]
-                ]
-            )
-        )]
-        function() {
-            return [
-                [
-                    "email"    => "some@email.com",
-                    "name"     => "name1",
-                    "articles" => [],
-                ],
-                [
-                    "email"    => "someother@email.com",
-                    "name"     => "name2",
-                    "articles" => [
-                        [
-                            "title"       => "article title 1",
-                            "description" => "article description 1",
-                        ],
-                        [
-                            "title"       => "article title 2",
-                            "description" => "article description 2",
-                        ],
-                    ],
-                ],
-            ];
-        }
-    );
-    
-
-
+    Route::get('/test', fn() => "this is a test");
     Route::get("/openapi", fn(OpenAPIService $oa) => $oa->getData());
     echo Route::describe().PHP_EOL;
 }
