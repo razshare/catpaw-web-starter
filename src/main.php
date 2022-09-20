4686f693cdb147e60edac0d4929f8a487a70239f
Razvan Tanase <tangent.jotey@gmail.com>
2022-09-21 00:43:54 +0200

    Version 0.4.18

--- src/main.php
+++ src/main.php
@@ -1,5 +1,6 @@
 <?php
 
+use CatPaw\Web\Attributes\Example;
 use CatPaw\Web\Attributes\Param;
 use CatPaw\Web\Attributes\ProducedResponse;
 use CatPaw\Web\Attributes\Produces;
@@ -9,18 +10,19 @@ use CatPaw\Web\Utilities\Route;
 
 #[StartWebServer]
 function main() {
-    // Route::get(
-    //     '/test/{value}',
-    //     #[Produces("text/plain", new ProducedResponse(example:[
-    //         "test" => "test"
-    //     ]))]
-    //     function(
-    //         #[Param(example:'test')] string $value
-    //     ) {
-    //         return "this is a test and the value is: $value";
-    //     }
-    // );
+    Route::get(
+        '/test/{value}',
+        #[Produces("text/plain", new ProducedResponse(example:[
+            "test" => "test"
+        ]))]
+        function(
+            #[Example('this is an example value')] #[Param] string $value
+        ) {
+            return "this is a test and the value is: $value";
+        }
+    );
     
+
 
     Route::get("/openapi", fn(OpenAPIService $oa) => $oa->getData());
     echo Route::describe().PHP_EOL;
