6185ae0a761ee30708f9ac7192eecefae592ef7a
tncrazvan <tangent.jotey@gmail.com>
2023-06-16 04:10:14 +0200

    Version 0.5.7

--- src/main.php
+++ src/main.php
@@ -1,13 +1,14 @@
 <?php
 
 use CatPaw\Environment\Attributes\Environment;
-use CatPaw\Web\Attributes\StartWebServer;
+use CatPaw\Web\Server;
 use CatPaw\Web\Services\OpenAPIService;
 use CatPaw\Web\Utilities\Route;
 
 #[Environment]
-#[StartWebServer]
-function main() {
+function main():void {
     Route::get("/openapi", fn (OpenAPIService $oa) => $oa->getData());
+    $server = Server::create();
     echo Route::describe().PHP_EOL;
+    $server->start();
 }
