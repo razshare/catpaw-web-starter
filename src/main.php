2dbfc3ae549bc91ae207da1b028f748fd2b12584
tncrazvan <tangent.jotey@gmail.com>
2023-06-19 17:54:29 +0200

    Version 0.7.56

--- src/main.php
+++ src/main.php
@@ -3,7 +3,6 @@
 use CatPaw\Web\Attributes\IgnoreOpenAPI;
 use CatPaw\Web\Server;
 use CatPaw\Web\Services\OpenAPIService;
-use CatPaw\Web\Utilities\Route;
 
 function main():void {
     Route::get("/openapi", #[IgnoreOpenAPI] fn (OpenAPIService $oa) => $oa->getData());
