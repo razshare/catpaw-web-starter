5f8601634c2e871ff8c527c5f72881c62f17cc23
Razvan Tanase <tangent.jotey@gmail.com>
2023-07-13 00:59:50 +0200

    removing some test echos

--- src/main.php
+++ src/main.php
@@ -5,7 +5,6 @@ use CatPaw\Web\Server;
 use CatPaw\Web\Services\OpenAPIService;
 
 function main():void {
-    echo json_encode(['test' => $_ENV['test']]).PHP_EOL;
     $server = Server::create();
     $server->router->get('/openapi', #[IgnoreOpenAPI] fn (OpenAPIService $oa) => $oa->getData());
     $server->start();
