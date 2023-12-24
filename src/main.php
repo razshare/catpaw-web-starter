3cbe4310d60b1451946ce45df71cfe8e2060fe0f
tncrazvan <tangent.jotey@gmail.com>
2023-12-24 15:08:22 +0100

    chore(app): fixing some names

--- src/main.php
+++ src/main.php
@@ -2,10 +2,10 @@
 
 use CatPaw\Web\Attributes\IgnoreOpenAPI;
 use CatPaw\Web\Server;
-use CatPaw\Web\Services\OpenAPIService;
+use CatPaw\Web\Services\OpenApiService;
 
 function main():void {
     $server = Server::create();
-    $server->router->get('/openapi', #[IgnoreOpenAPI] fn (OpenAPIService $oa) => $oa->getData());
+    $server->router->get('/openapi', #[IgnoreOpenAPI] fn (OpenApiService $oa) => $oa->getData());
     $server->start();
 }
