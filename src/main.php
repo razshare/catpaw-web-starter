2730a662d9930c9c175631b2ad00f61d64f2b333
Razvan <tangent.jotey@gmail.com>
2024-01-01 01:50:38 +0100

    feat(docker): adding docker helpers

--- src/main.php
+++ src/main.php
@@ -5,7 +5,7 @@ use CatPaw\Web\Server;
 use CatPaw\Web\Services\OpenApiService;
 
 function main():void {
-    $server = Server::create();
+    $server = Server::create(interfaces:"0.0.0.0:5757");
     $server->router->get('/openapi', #[IgnoreOpenAPI] fn (OpenApiService $oa) => $oa->getData());
     $server->start();
 }
