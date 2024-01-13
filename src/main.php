50e6ab87eddd6c4bb40e67c72b623a4dc3713a0e
Razvan <tangent.jotey@gmail.com>
2024-01-13 06:18:19 +0100

    feat(pre-commit): adding pre-commit to the project

--- src/main.php
+++ src/main.php
@@ -8,28 +8,34 @@ use CatPaw\Web\Attributes\IgnoreOpenApi;
 use CatPaw\Web\Server;
 use CatPaw\Web\Services\OpenApiService;
 
+
 /**
  * @return Unsafe<void>
  */
-function main(OpenApiService $oa):Unsafe {
-    $oa->setTitle("My api");
-    $oa->setVersion("1.0.0");
-    return anyError(
+function main(OpenApiService $oa) {
+    return anyError(function() use ($oa) {
+        $oa->setTitle("My api");
+        $oa->setVersion("1.0.0");
+
         $server = Server::create(
             interface: env('interface'),
             www: env('www'),
             api: env('api'),
             apiPrefix: env('apiPrefix'),
-        ),
+        )
+            ->try($error)
+        or yield $error;
+
         $server
-            ->value
             ->router
-            ->get(
-                path:'/openapi',
-                function:
-                #[IgnoreOpenApi]
-                static fn () => $oa->getData(),
-            ),
-        $server->value->start()->await(),
-    );
+            ->get(path:'/openapi', function: #[IgnoreOpenApi] static fn () => $oa->getData())
+            ->try($error)
+            or yield $error;
+
+        $server
+            ->start()
+            ->await()
+            ->try($error)
+            or yield $error;
+    });
 }
