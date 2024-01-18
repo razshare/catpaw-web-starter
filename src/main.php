73061b569dc3064c8ed90e7811de6eaae3b6dbef
Razvan <tangent.jotey@gmail.com>
2024-01-18 20:32:20 +0100

    chore(app): fixing indentation

--- src/main.php
+++ src/main.php
@@ -24,7 +24,7 @@ function main(OpenApiService $oa) {
             apiPrefix: env('apiPrefix'),
         )
             ->try($error)
-        or yield $error;
+            or yield $error;
 
         $server
             ->router
