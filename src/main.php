04791d9f2902bc39c62754a00c4f9aab4f33e98d
Razvan Tanase <tangent.jotey@gmail.com>
2024-05-25 00:14:49 +0200

    chore(app): updating to catpaw 3.3.1

--- src/main.php
+++ src/main.php
@@ -4,12 +4,13 @@ use function CatPaw\Core\anyError;
 use function CatPaw\Core\asFileName;
 use function CatPaw\Core\env;
 use CatPaw\Core\Unsafe;
+
 use const CatPaw\Web\APPLICATION_JSON;
 use CatPaw\Web\Attributes\IgnoreOpenApi;
 use CatPaw\Web\Interfaces\ResponseModifier;
+use function CatPaw\Web\loadComponent;
 use CatPaw\Web\Server;
 use CatPaw\Web\Services\OpenApiService;
-use CatPaw\Web\Services\TwigService;
 
 use function CatPaw\Web\success;
 
@@ -21,12 +22,12 @@ function openapi(OpenApiService $oa):ResponseModifier {
 /**
  * @return Unsafe<void>
  */
-function main(OpenApiService $oa, TwigService $twig): Unsafe {
-    return anyError(function() use ($oa, $twig) {
-        $twig->loadComponent(asFileName(__DIR__, 'index.twig'), 'index')->try();
-
+function main(OpenApiService $oa): Unsafe {
+    return anyError(function() use ($oa) {
         $oa->setTitle("My Api");
         $oa->setVersion("1.0.0");
+        
+        loadComponent(asFileName(__DIR__, 'index.twig'), 'index')->try();
 
         $server = Server::get()
             ->withInterface(env('interface'))
