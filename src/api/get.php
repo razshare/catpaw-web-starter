5451e95113a22fcdfeacd892f154f9ca3ecd8258
Razvan <razvan@razshare.dev>
2024-10-27 21:23:32 +0100

    chore(app): upgrading to catpaw 4

--- src/api/get.php
+++ src/api/get.php
@@ -1,3 +1,2 @@
 <?php
-use function CatPaw\Web\view;
-return fn () => view()->withProperty("title", "My Website");
+return fn () => "hello";
--- src/main.php
+++ src/main.php
@@ -1,16 +1,11 @@
 <?php
-use function CatPaw\Core\anyError;
-use function CatPaw\Core\asFileName;
 use function CatPaw\Core\env;
-use CatPaw\Core\Unsafe;
 use const CatPaw\Web\APPLICATION_JSON;
 use CatPaw\Web\Attributes\IgnoreOpenApi;
 use CatPaw\Web\Interfaces\OpenApiInterface;
 use CatPaw\Web\Interfaces\ResponseModifier;
 use CatPaw\Web\Interfaces\RouterInterface;
 use CatPaw\Web\Interfaces\ServerInterface;
-use CatPaw\Web\Interfaces\ViewEngineInterface;
-use function CatPaw\Web\loadComponentFromFile;
 use function CatPaw\Web\success;
 
 #[IgnoreOpenApi]
@@ -18,30 +13,19 @@ function openapi(OpenApiInterface $openApi):ResponseModifier {
     return success($openApi->data())->as(APPLICATION_JSON);
 }
 
-/**
- * @return Unsafe<void>
- */
 function main(
     ServerInterface $server,
     RouterInterface $router,
     OpenApiInterface $openApi,
-    ViewEngineInterface $viewEngine,
-): Unsafe {
-    return anyError(function() use ($server, $router, $openApi, $viewEngine) {
-        $viewEngine->withTemporaryDirectory(asFileName(__DIR__, '.tmp'));
-        $openApi->withTitle("My Api");
-        $openApi->withVersion("1.0.0");
-
-        loadComponentFromFile('index', [], asFileName(__DIR__, 'index.latte'))->try();
-
-        $router->get('/openapi', openapi(...))->try();
-
-        $server
-            ->withInterface(env('interface'))
-            ->withStaticsLocation(env('staticsLocation'))
-            ->withApiLocation(env('apiLocation'))
-            ->withApiPrefix('/')
-            ->start()
-            ->try();
-    });
+): void {
+    $openApi->withTitle("My Api");
+    $openApi->withVersion("1.0.0");
+    $router->get('/openapi', openapi(...))->try();
+    $server
+        ->withInterface(env('interface'))
+        ->withStaticsLocation(env('staticsLocation'))
+        ->withApiLocation(env('apiLocation'))
+        ->withApiPrefix('/')
+        ->start()
+        ->try();
 }
