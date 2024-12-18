f53371b9a997680e581ba7341416e0bb31f3981c
Razvan Tanase <razvan@razshare.dev>
2024-12-18 05:19:39 +0100

    chore(app): removing swaggerui and adding tailwindcss.

--- api/get.php
+++ api/get.php
@@ -1,2 +1,29 @@
 <?php
-return static fn () => 'Hello from api!';
+use CatPaw\Web\QueryItem;
+use function CatPaw\Web\render;
+?>
+
+<?php return static fn (QueryItem $name) => render(function() use ($name) { ?>
+    <?php $name = $name->text() ?: 'world'; ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome</title>
+        <link rel="stylesheet" href="/index.css">
+    </head>
+    <body>
+        <div class="
+            fixed left-0 right-0 top-0 bottom-0 grid
+            justify-center content-center
+            bg-gray-900
+            text-8xl
+        ">
+            <h3 class="text-orange-600">
+                Hello, <?=$name?>.
+            </h3>
+        </div>
+    </body>
+    </html>
+<?php }) ?>
--- src/main.php
+++ src/main.php
@@ -1,40 +1,23 @@
 <?php
+use function CatPaw\Core\asFileName;
 use function CatPaw\Core\env;
-use function CatPaw\Core\error;
+use function CatPaw\Core\execute;
 use CatPaw\Core\None;
 use CatPaw\Core\Result;
-use const CatPaw\Web\APPLICATION_JSON;
-use CatPaw\Web\Attributes\IgnoreOpenApi;
-use CatPaw\Web\Interfaces\OpenApiInterface;
-use CatPaw\Web\Interfaces\ResponseModifier;
-use CatPaw\Web\Interfaces\RouterInterface;
 use CatPaw\Web\Interfaces\ServerInterface;
-use function CatPaw\Web\success;
 
-#[IgnoreOpenApi]
-function openapi(OpenApiInterface $openApi):ResponseModifier {
-    return success($openApi->data())->as(APPLICATION_JSON);
-}
+
 
 /**
  * Application entry point.
- * @param  ServerInterface  $server
- * @param  RouterInterface  $router
- * @param  OpenApiInterface $openApi
+ * @param  ServerInterface $server
  * @return Result<None>
  */
-function main(
-    ServerInterface $server,
-    RouterInterface $router,
-    OpenApiInterface $openApi,
-): Result {
-    $openApi->withTitle("My Api");
-    $openApi->withVersion("1.0.0");
-    
-    $router->get('/openapi', openapi(...))->unwrap($error);
-    if ($error) {
-        return error($error);
-    }
+function main(ServerInterface $server): Result {
+    $input  = asFileName(__DIR__, 'main.css')->absolute();
+    $output = asFileName(__DIR__, '../statics/main.css')->absolute();
+
+    execute("asd tailwindcss -i $input -o $output")->logError();
 
     return $server
         ->withInterface(env('interface'))
